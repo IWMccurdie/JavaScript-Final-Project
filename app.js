6cb1addcef86d92ee8dc917118a6c07c3a14fe35
skhatri7 <saugatsuz77@gmail.com>
2025-12-03 11:23:27 -0600

    Added UI and backend server changes

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+const port=3000
+
+//middleware
+app.use(express.static('public'))
+app.use(express.json())
+
+//routes
+// app.use('/api/v1',require('./routes/api/v1/menu'))
+// app.use('/api/v1',require('./routes/api/v1/events'))
+app.use('/',require('./routes/pages/menu'))
+
+//server
+const url='http://localhost:3000/'
+const message=`Server is running on port ${port}.Visit ${url}`
+app.listen(port,()=>console.log(message))
