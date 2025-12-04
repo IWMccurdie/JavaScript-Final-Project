2ef5e5c9caf5c76ef6e06337c7cf3159728111c4
skhatri7 <saugatsuz77@gmail.com>
2025-12-04 12:33:02 -0600

    Added api structure

---
+++ routes/api/v1/events.js
@@ -0,0 +1,18 @@
+const router=require('express').Router()
+const menus=require('../../../data/events.json')
+
+//apis
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=router
---
+++ routes/api/v1/menu.js
@@ -0,0 +1,18 @@
+const router=require('express').Router()
+const menus=require('../../../data/menu.json')
+
+//apis
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=router
