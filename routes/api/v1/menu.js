3e72db5018c4bffd170f9f1643943e801cc95096
mgfrascona <mgfrascona@gmail.com>
2025-12-11 12:22:10 -0600

    merge fix

--- routes/api/v1/menu.js
+++ routes/api/v1/menu.js
@@ -1,39 +1,20 @@
-<<<<<<< HEAD
 const router=require('express').Router()
 const menus=require('../../../data/menu.json')
 const menu = require('../../../models/Menu')
 
 //apis
 
-
-
-
-
-
-
-
-
+router.get('/menu/:id', (request, response) => {
+    const { id } = request.params
+    const found = menu.find(item => item.id.toString() === id)
+    if (found) return response.send(found)
+    response.status(400).json({ message: "Item on menu not found" })
+})
 
 //post route
 router.post('/menus',async(req,res)=>{
     const newitem=await menu.create(req.body)
     res.json(newitem)
-
 })
-
 
 module.exports=router
-=======
-const router = require('express').Router()
-const menu = require('../../../data/menu.json')
-
-router.get('/menu/:id', (request, response) => {
-    const { id } = request.params
-    const found = menu.find(item => item.id.toString() === id)
-    if (found) return response.send(found)
-    response.json(found)
-    response.status(400).json({ message: "Item on menu not found" })
-})
-
-module.exports = router
->>>>>>> mgfrascona
